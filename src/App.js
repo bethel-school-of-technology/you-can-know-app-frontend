6f65b1764e31815690aa0965c258af5fd2ee5ecf
joegartmane <redeemedroyalty14@gmail.com>
2021-09-06 12:52:03 -0300

    basics for signup and login pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, Redirect} from 'react-router-dom';
 import Home from './components/Home';
+import SignUp from './components/pages/SignUp';
+import Login from './components/pages/Login';
 
 const App = () => {
 //const title = 'You Can Know!'
@@ -14,6 +16,8 @@ const App = () => {
         <Route exact path="/">
         <Redirect to="/home" />
         </Route>
+        <Route path='/signup' component={SignUp} />
+        <Route path='/login' component={Login} />
       </Switch>
     
   );
---
+++ src/components/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className="container">
+            <h1>Please Login</h1>
+            <label>Username:</label>
+            <input></input>
+            <br></br>
+            <label>Password:</label>
+            <input></input>
+            <br></br>
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default Login
