ddb114382accd3cc57ab8379f5d8f2fda5ff7dc6
A. Sherry <asherry.betheltech777@gmail.com>
2021-09-09 12:14:04 -0700

    created & added searchbar

--- src/index.js
+++ src/index.js
@@ -12,3 +12,9 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+// app.get("/*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
